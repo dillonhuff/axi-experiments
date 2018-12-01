fbcc5b0111872653fa7fec38ccd0b6134469b177
Dillon Huff <dillonhuff@gmail.com>
2018-12-01 13:41:14 -0800

    Wrapping up axi write functionality in handlers that make creating them easier

--- top.v
+++ top.v
@@ -1,3 +1,76 @@
+// Q: I can get the slave to write data and read data, how do I encapsulate
+//    this in to something I can use in my HLS tool?
+// A: Maybe create write modules and communicate modules?
+//    1. Assume write data and valid show up at the same time
+//    2. Handle waiting for the write response to return
+
+module axi_write_handler(input clk,
+                        input                           rst,
+
+                        input [DATA_WIDTH - 1 : 0]      write_data,
+                        input [ADDR_WIDTH - 1 : 0]      write_addr,
+                        input                           start_write,
+
+                        output                          ready,
+
+   //output [ADDR_WIDTH-1:0] s_axil_awaddr;
+                        output reg                      s_axil_awvalid,
+   // reg [DATA_WIDTH-1:0] s_axil_wdata;
+   // reg [STRB_WIDTH-1:0] s_axil_wstrb;
+
+                        output reg                      s_axil_wvalid,
+
+                        output reg [DATA_WIDTH - 1 : 0] s_axil_wdata,
+                        output reg [ADDR_WIDTH - 1 : 0] s_axil_waddr
+
+   // reg                  s_axil_bready;
+
+   // reg [ADDR_WIDTH-1:0] s_axil_araddr;
+
+   // reg                  s_axil_arvalid;
+
+   // reg                   s_axil_rready;
+
+   
+   // wire                 s_axil_awready;
+   // wire                 s_axil_wready;
+   // wire [1:0]           s_axil_bresp;
+   // wire                 s_axil_bvalid;
+
+                        );
+
+   parameter DATA_WIDTH = 32;
+   parameter ADDR_WIDTH = 5;
+   parameter STRB_WIDTH = (DATA_WIDTH/8);
+
+   reg                         ready_reg;
+
+   assign ready = ready_reg;
+
+   // s_axil_awaddr = 1;
+
+   // s_axil_wdata = 2345;
+
+   // s_axil_wstrb = 5'b11111;
+   
+   always @(posedge clk) begin
+      if (rst) begin
+         ready_reg <= 1;
+
+         s_axil_wvalid <= 0;
+         s_axil_awvalid <= 0;
+      end else if (start_write) begin
+         s_axil_wvalid <= 1;
+         s_axil_awvalid <= 1;
+
+         s_axil_wdata <= write_data;
+         s_axil_waddr <= write_addr;
+
+      end
+   end
+
+endmodule
+
 module top();
 
    parameter DATA_WIDTH = 32;
